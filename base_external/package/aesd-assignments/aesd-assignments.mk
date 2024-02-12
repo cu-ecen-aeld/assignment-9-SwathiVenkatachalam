f25e490585baf2ae0562dbcfe8121fe6e6022d02
swathi <swathi@Ubuntu.myguest.virtualbox.org>
2024-02-11 17:22:48 -0700

    Assignment 3 finder-test.sh updated so commit updated in .mk file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'bae3006beb13c3a39856e103a5ce4542b3701738'
+AESD_ASSIGNMENTS_VERSION = '98c93390fcdc06db8ef6a2ffaaf9675004a931d3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
