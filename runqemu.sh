e7f4e0fd040a8c8a54b9abdae30d148fcc50439f
swathi <swathi@Ubuntu.myguest.virtualbox.org>
2024-02-26 09:56:03 -0700

    Restarting actions runner by deleting runner

--- runqemu.sh
+++ runqemu.sh
@@ -3,7 +3,7 @@
 #Host forwarding: Host Port 10022 ->> QEMU Port 22 
 #Author: Siddhant Jajoo.
 
-
+#A5 P2: forward host port 9000 to your qemu instance port 9000. Also to pass through virtual machine port 10022 to port 22 on your qemu instance.
 qemu-system-aarch64 \
     -M virt  \
     -cpu cortex-a53 -nographic -smp 1 \
@@ -13,3 +13,5 @@ qemu-system-aarch64 \
     -device virtio-net-device,netdev=eth0 \
     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+    
+    
